b0f5464a0c4c76e7c1b922998473084d117acb84
Clyde <Clyde@CLYDE-PC>
2017-08-14 11:55:34 +0100

    Fixed corp loading URI

---
+++ R3MUS.Devpack.ESI.Tests/CorpTests/CorpExtTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R3MUS.Devpack.ESI.Models.Corporation;
+
+namespace R3MUS.Devpack.ESI.Tests.CorpTests
+{
+    [TestClass]
+    public class CorpExtTests
+    {
+        [TestMethod]
+        public void GetCorp_ReturnValid()
+        {
+            //  Arrange
+            var corpId = 98522858;
+            //  Act
+            var corpResponse = new Detail(corpId);
+
+            //  Assert
+            corpResponse.Should().NotBeNull();
+        }
+    }
+}
--- R3MUS.Devpack.ESI/Extensions/CorporationExt.cs
+++ R3MUS.Devpack.ESI/Extensions/CorporationExt.cs
@@ -8,7 +8,7 @@ namespace R3MUS.Devpack.ESI.Extensions
     {
         public static void GetCorporation(this Detail me)
         {
-            var reqUri = string.Format("{0}/{1}/{2}/{3}", Constants.BaseURI, Constants.Corporations, me.Id.ToString(), Constants.BaseURITail);
+            var reqUri = string.Format("{0}/{1}/{2}/?{3}", Constants.BaseURI, Constants.Corporations, me.Id.ToString(), Constants.BaseURITail);
             var obj = (Detail)Web.BaseRequest(reqUri).Deserialize(typeof(Detail));
             me.SetProperties(obj);
         }
